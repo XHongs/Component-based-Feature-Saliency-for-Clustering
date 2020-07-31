d119d00505567806f523b4392c1bfc723aaad2a4
Xin Hong <58267310+XHongs@users.noreply.github.com>
2020-07-31 16:08:03 +0100

    Update UpdatezbetaKM.m

--- UpdatezbetaKM.m
+++ UpdatezbetaKM.m
@@ -1,3 +1,23 @@
+% Copyright (c) 2019 CSIT, Queen's University Belfast, UK
+% Contact: x.hong@qub.ac.uk
+% If you use this code please cite:
+% "Component-based Feature Saliency for Clustering",
+% X Hong, H Li, P Miller, J Zhou, L Li, D Crookes, Y Lu, X Li, H Zhou, 
+% IEEE Transactions on Knowledge and Data Engineering, 2019 
+% 
+% This software is licensed for research and non-commercial use only.
+% 
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
+
 function [zz,z,beta,nk,nm,nkm]=UpdatezbetaKM(I,K,M,y,...
     alpha,phokm,mu,sigma)
 % 
